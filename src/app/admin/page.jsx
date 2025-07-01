1e3abe398ae6ccc69f9b5c412f694867a4a1dcba
Pratyush <choudharypratyush809@gmail.com>
2025-07-02 03:44:32 +0530

    minor changes

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -9,7 +9,7 @@ export default async function AdminPage() {
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
-    redirect("/login");
+    redirect("/");
   }
 
   await connectMongoDb();
