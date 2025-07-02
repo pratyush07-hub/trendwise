ae332aadc74618b8a4da44ec858c654dc42bd22d
Pratyush <choudharypratyush809@gmail.com>
2025-07-02 12:31:22 +0530

    bug fixed

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,7 @@ const nextConfig = {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose'],
-  },
+  serverExternalPackages: ['mongoose'], 
 };
 
 export default withFlowbiteReact(nextConfig);
